9a6af96257f73dffcd950c662f29202cc2a4a7fb
Sunset-Kim <77092632+Sunset-Kim@users.noreply.github.com>
2021-11-15 23:34:51 +0900

    🔧  add firebase config file

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -3,9 +3,9 @@ import { initializeApp } from "firebase/app"
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_FIREBASE_APPID
 };
 
